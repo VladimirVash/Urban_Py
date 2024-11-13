2c8f9280f00b2ef479214bcca4b07cfd8406d40b
VladimirVash <volodka2601@gmail.com>
2024-11-13 11:56:54 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Vladimir'
+print(f'Name: {name}')
+age = 19
+print(f'Age: {age}')
+age = age + 1            # <-здесь у меня есть вопрос. Правильно ли?
+print(f'New Age: {age}')
+is_student = True
+print(f'Is Student: {is_student}')
