cadbad8d6c905b8e5e258478e4a20c92845af05e
VladimirVash <volodka2601@gmail.com>
2024-11-14 12:48:40 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+tasks_done = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / tasks_done
+print(f'\nНа курсе {course_name} было решено {tasks_done} заданий.'
+      f'\nПотречено {hours_spent} часов.'
+      f'\nВремя на выполнение 1 задачи: {time_per_task} часов.')
