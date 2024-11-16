f4d0426c26c75cd45714163d93227f57679126fd
VladimirVash <volodka2601@gmail.com>
2024-11-16 12:20:16 +0300

    Add files via upload

---
+++ module_1_12.py
@@ -0,0 +1,8 @@
+my_string = input()
+
+print(len(my_string),'\n',
+      my_string.upper(),'\n',
+      my_string.lower(),'\n',
+      my_string.replace(' ', ''),'\n',
+      my_string[0],'\n',
+      my_string[-1], sep='')
